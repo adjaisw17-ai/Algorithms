2fe25466f0e38dfd5b039fd9a4fbb2682977fcdf
adjaisw17-ai <adjaisw17@gmail.com>
2026-09-23 15:13:32 +0530

    Add Insertion Sort implementation in C++
    This file implements the Insertion Sort algorithm in C++. It includes functions for sorting an array and printing the sorted array.

---
+++ Sorting/InsertionSort.cpp
@@ -0,0 +1,45 @@
+// C++ program for implementation of Insertion Sort
+#include <iostream>
+using namespace std;
+
+// Best case: O(n), If the list is already sorted, where n is the number of elements in the list.
+// Average case: O(n2), If the list is randomly ordered
+// Worst case: O(n2), If the list is in reverse order
+
+/* Function to sort array using insertion sort */
+void insertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; ++i) {
+        int key = arr[i];
+        int j = i - 1;
+
+        /* Move elements of arr[0..i-1], that are
+           greater than key, to one position ahead
+           of their current position */
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+/* A utility function to print array of size n */
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; ++i)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+// Driver method
+int main()
+{
+    int arr[] = { 12, 11, 13, 5, 6 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    insertionSort(arr, n);
+    printArray(arr, n);
+
+    return 0;
+}
